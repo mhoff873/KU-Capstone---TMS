94af352e4e7e0097b1fbbd4e8d2834e7bcdb1003
David Schaeffer <dscha959@live.kutztown.edu>
2018-01-30 13:55:48 -0500

    Forced committing config.py since it doesn't have any real data anyway.

---
+++ config.py
@@ -0,0 +1,11 @@
+# Flask and many of it's plugins require a number of system variables to be set
+# This file is used to define those variables and are loaded in app.py.
+
+# Database Configs
+# See: http://flask-mysqldb.readthedocs.io/en/latest/
+MYSQL_HOST = ''
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
+MYSQL_DB = ''
+MYSQL_PORT = ''
+MYSQL_CONNECT_TIMEOUT = 20
