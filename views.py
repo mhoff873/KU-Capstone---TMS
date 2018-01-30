402f1ed4edc80a590adfe7515bacdd4fb37620eb
David Schaeffer <dscha959@live.kutztown.edu>
2018-01-30 13:49:07 -0500

    Added some db pseudocode to poke flask-mysql.
    I feel it'd be best if we could get DB connection info from DB team and then test this library out. If it fits our needs, we're good to go. Otherwise we'll have to look at using jQuery AJAX embedded within our python to do our connections or try to find another python3 sql lib.

--- views.py
+++ views.py
@@ -5,4 +5,15 @@ from app import app
 
 @app.route('/', methods=['GET'])
 def index():
+    """
+    Example flask-mysqldb implementation
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT * FROM users''')
+    NOTE: ^ is so so so so so unsecure. SQL injections yall.
+    results = cursor.fetchall()
+    return results
+
+    If this lib doesn't have all the support we need, we'll need to look at
+    using jQuery AJAX embedded within our python. 
+    """
     return render_template('index.html')
