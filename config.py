f7b8ffcca0b0399f8a899c9dbd6849361ed57f04
David Schaeffer <dscha959@live.kutztown.edu>
2018-02-01 09:52:09 -0500

    Added note to config about what the secret key is for.

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 # This file is used to define those variables and are loaded in app.py.
 
 # App Config
+# Secret Key is required for the CSRF tokens generated in our forms. It's a
+# security feature for the forms.
 SECRET_KEY = 'wefb292h3d9be#@@YEBBCE2NION32UDFEBUE2U202hfeu2onwdsjdf'
 
 # Database Configs
