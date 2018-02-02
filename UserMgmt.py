3a7fce76ebf900c6d7166d47020c7ed76dd4dbbb
Nathan Yost <nathanyost15@gmail.com>
2018-02-01 20:19:11 -0500

    Added skeleton for functions related to requirements 27-31

---
+++ UserMgmt.py
@@ -0,0 +1,13 @@
+def create_user(acountType):
+    """Fulfill requirements 27, 28"""
+    pass
+
+
+def edit_user(user):
+    """Fulfill requirement(s) 29, 30"""
+    pass
+
+
+def assign_user(assignee, assigned):
+    """Fulfill requirement(s) 31"""
+    pass
