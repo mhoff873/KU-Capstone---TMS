a7a234ab61d4ccd314582598ef2343b6f0fe67af
Aaron M. Klinikowski <redk13@gmail.com>
2018-02-07 10:59:02 -0500

    Added Main Step Fields for the Test Page

--- forms.py
+++ forms.py
@@ -4,7 +4,15 @@ them easier to handle and implement.
 """
 from flask_wtf import FlaskForm
 from wtforms import SelectField, IntegerField, FieldList, StringField
+from flask_wtf.file import FileField
 
 
 class CreateTaskForm(FlaskForm):
     task_name = StringField('Task Name')
+
+    #Main Steps
+    main_step_image = FileField('Upload Image for Main Step')
+    main_step_name = StringField('Title of Main Step')
+    main_step_text = StringField('Main Step Description')
+    main_step_items = StringField('Required Items')
+    main_step_media = FileField('Upload Audio/Video')
