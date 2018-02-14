e1dbe2ac1e8b5e5d96d1e2bf2f8a04836ff9ea7c
DylanKramer <35779385+DylanKramer@users.noreply.github.com>
2018-02-14 15:46:50 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
-from flask_mysqldb import MySQL
-
-from app import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # This connects to the DB. Obviously we only want to connect when required, so
 # this will be of interest to Nate and his DB interface class.
-mysql = MySQL(app)
+
+# need? 
+# app = Flask(__name__)
+
+# user and password need to be defined somehow
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost'
+db = SQLAlchemy(app)
 
 # flask-mysqldb doesn't seem to handle sanitization of variables used in queries,
 # we will need to define a way to handle that for security reasons.
