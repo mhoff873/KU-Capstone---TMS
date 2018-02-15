76d0284984fca39b0c13fd031464f90b2945d16b
David Schaeffer <david.schaeffer2@gmail.com>
2018-02-15 10:20:22 -0500

    Resolved error.

--- views.py
+++ views.py
@@ -24,15 +24,15 @@ def index():
 def create_task():
     num_detailed_steps = 1
     form = CreateTaskForm()
-
-    if request.form['submit'] == 'add_detailed_step':
-        num_detailed_steps += 1
-        return render_template('create_task.html', form=form, num_detailed=num_detailed_steps)
-    elif request.form['submit'] == 'save_as_draft':
-        # User clicked save as draft button
-        pass
-    elif request.form['submit'] == 'publish':
-        # User clicked publish button
-        pass
+    if request.method == 'POST':
+        if request.form['submit'] == 'add_detailed_step':
+            num_detailed_steps += 1
+            return render_template('create_task.html', form=form, num_detailed=num_detailed_steps)
+        elif request.form['submit'] == 'save_as_draft':
+            # User clicked save as draft button
+            pass
+        elif request.form['submit'] == 'publish':
+            # User clicked publish button
+            pass
     elif request.method == 'GET':
         return render_template('create_task.html', form=form, num_detailed=num_detailed_steps)
