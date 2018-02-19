89ee5f358825f82f45346f8e38e57bf4c1665f03
David Schaeffer <david.schaeffer2@gmail.com>
2018-02-19 12:17:45 -0500

    Can now add main steps and detailed steps for each main step.

--- views.py
+++ views.py
@@ -13,6 +13,9 @@ def index():
 def create_task():
     form = CreateTaskForm()
     print(form.data)
+    print(request.method)
+    if request.method == ['GET']:
+        return render_template('create_task.html', form=form)
     if form.add_main_step.data:
         print('Add main step')
         form.main_step.append_entry()
@@ -23,4 +26,11 @@ def create_task():
     elif form.publish.data:
         print('Publish Task')
         return render_template('index.html')
+    else:
+        print('Checking for detailed step button press.')
+        for i, step in enumerate(form.main_step):
+            print(i)
+            if step['add_detailed_step']:
+                step.detailed_steps.append_entry()
+                print(f'Add detailed for main step {i}.')
     return render_template('create_task.html', form=form)
