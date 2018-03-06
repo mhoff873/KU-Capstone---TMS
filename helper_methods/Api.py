459558bd0b796808ca76c7c262555e603c35f97a
Team Database <team_db@tmst.kutztown.edu>
2018-03-06 09:15:13 -0500

    Completed getAllCompletedSteps

--- helper_methods/Api.py
+++ helper_methods/Api.py
@@ -6,6 +6,7 @@
 # Functions relating to the API 
 
 import bcrypt
+from datetime import datetime
 from database import * 
 from Forms.models import User
 
@@ -29,6 +30,54 @@ def getHash(email):
     p = (User.query.filter_by(email=email).first())
     return p if p else None
 
+def getByUser(uname):
+    '''
+        Function:       getbyuser
+        Purpose:        Gets the User's tasks assigned to them
+        Return:         json - A json object containing the tasks for the user
+        Author:         Tyler Lance
+    '''
+    # print("USERNAME LOGGED IN:" + uname)
+    # cursor documentation: http://initd.org/psycopg/docs/cursor.html
+    cur = mysql.connection.cursor()
+    # get the accounts userID
+    cur.execute('SELECT userID FROM users WHERE email=%s', [uname])
+    # pull the userId from the cursor
+    result = cur.fetchone()  # returns as ((#,),)
+    print(result)
+    # Invalid user
+    if result is None:
+        return jsonify([])
+
+    userID = result['userID']  # stores the first element which is the userid
+    # get the tasks assigned to the user
+    cur.execute(
+        'SELECT task.taskID, task.title FROM task, users, request WHERE users.userID=%s AND users.userID=request.userID AND request.taskID=task.taskID',
+        (userID,))
+    record = cur.fetchall()
+
+    # formatting of the api, has fixed values for the category name and id
+    # because they dont exist in the TMS database
+    result = {"$id": "1", "categoryName": "testing", "categoryId": 1}
+    lst = []  # list to append each task into
+    count = 2  # for the id of each element, as per the api
+    # build up the list
+
+    for index in record:
+        # Don't assign a keyword..
+        tsks = {}
+        if app.config['MYSQL_CURSORCLASS'] == 'DictCursor':
+            # create dictionary of the id, taskid and taskname
+            tsks = {"$id": str(count), "taskId": index['taskID'],
+                    "taskName": index['title']}
+        else:
+            tsks = {"id": str(count), "taskId": index[0], "taskName": index[1]}
+        # append the dictionary to the list
+        lst.append(tsks)
+        count += 1
+    # add the list to the dictionary with the key being tasks
+    result["tasks"] = lst
+    return result
     
 # Returns the details for a specfic task 
 def getTaskDetails(taskid):
@@ -42,7 +91,7 @@ def getTaskDetails(taskid):
     count = 1
     
     for r in step:
-        print(count)
+
         s = {"$id" : str(count), "mainStepName" : r['title'], "mainStepId": r['mainStepID'], 
             "mainStepText" : r['stepText'], "videoPath": r['video'], "audioPath": r['audio'],
             "requiredItems" : r['requiredItem'] }
@@ -70,4 +119,18 @@ def getTaskDetails(taskid):
 # Returns all the steps by a given user 
 def getAllCompletedSteps(uname, taskid):
     cur = mysql.connection.cursor()
+    cur.execute('''SELECT mainSteps.title, users.email, completedSteps.dateTimeCompleted, completedSteps.timeSpent
+                FROM mainSteps, users, completedSteps
+                WHERE completedSteps.mainStepID = mainSteps.mainStepID AND completedSteps.userID = users.userID AND mainSteps.taskID = %d AND users.email = "%s" AND completedSteps.dateTimeCompleted IS NOT NULL''' % (int(taskid), uname, ))
+    r = cur.fetchall()
+    count = 1
+    lst = []
+    for s in r:
+        date_string = str(s["dateTimeCompleted"])
+        spc = str(s["dateTimeCompleted"]).find(' ')
+        ufc = date_string[:spc] + "T" + date_string[spc+1:]
+        s = {"$id" : str(count), "mainStepName" : s["title"], "assignedUser" : s["email"], "dateTimeCompleted" : ufc, "totalTime" : s["timeSpent"]}
+        lst.append(s)
+        count += 1
+    return lst
     
--- views.py
+++ views.py
@@ -29,55 +29,8 @@ def api_login():
 
 @app.route('/api/user/GetByUser/<uname>', methods=['GET'])
 def api_getbyuser(uname):
-    '''
-        Function:       getbyuser
-        Purpose:        Gets the User's tasks assigned to them
-        Return:         json - A json object containing the tasks for the user
-        Author:         Tyler Lance
-    '''
-    # print("USERNAME LOGGED IN:" + uname)
-    # cursor documentation: http://initd.org/psycopg/docs/cursor.html
-    cur = mysql.connection.cursor()
-    # get the accounts userID
-    cur.execute('SELECT userID FROM users WHERE email=%s', [uname])
-    # pull the userId from the cursor
-    result = cur.fetchone()  # returns as ((#,),)
-    print(result)
-    # Invalid user
-    if result is None:
-        return jsonify([])
-
-    userID = result['userID']  # stores the first element which is the userid
-    # get the tasks assigned to the user
-    cur.execute(
-        'SELECT task.taskID, task.title FROM task, users, request WHERE users.userID=%s AND users.userID=request.userID AND request.taskID=task.taskID',
-        (userID,))
-    record = cur.fetchall()
-
-    # formatting of the api, has fixed values for the category name and id
-    # because they dont exist in the TMS database
-    result = {"$id": "1", "categoryName": "testing", "categoryId": 1}
-    lst = []  # list to append each task into
-    count = 2  # for the id of each element, as per the api
-    # build up the list
-
-    for index in record:
-        # Don't assign a keyword..
-        tsks = {}
-        if app.config['MYSQL_CURSORCLASS'] == 'DictCursor':
-            # create dictionary of the id, taskid and taskname
-            tsks = {"$id": str(count), "taskId": index['taskID'],
-                    "taskName": index['title']}
-        else:
-            tsks = {"id": str(count), "taskId": index[0], "taskName": index[1]}
-        # append the dictionary to the list
-        lst.append(tsks)
-        count += 1
-    # add the list to the dictionary with the key being tasks
-    result["tasks"] = lst
-    result = jsonify([result])
-
-    return result
+    r = Api.getByUser(uname)
+    return jsonify([r])
 
 
 # For sprint 2
@@ -87,10 +40,11 @@ def getTaskDetails(taskid):
     results = Api.getTaskDetails(taskid)
     return jsonify(results)
 
-@app.route("/api/GetAllCompletedSteps/<uname>/<taskid>")
+@app.route("/api/user/GetAllCompletedSteps/<uname>/<taskid>")
 def getAllCompletedSteps(uname, taskid):
     results = Api.getAllCompletedSteps(uname, taskid)
     return jsonify(results)
+
 '''@app.route("/api/GetByUser/<uname>")
 def GetByUser(uname):
     return
