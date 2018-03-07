36b43930a59f063933ac37b25e90a9f64553500d
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-07 16:13:41 -0500

    Troubleshooting messages sprinkled in.

--- Forms/models.py
+++ Forms/models.py
@@ -8,7 +8,7 @@
 
 from database import db, login_manager
 from datetime import datetime
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user
 from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey, \
     Date
 from sqlalchemy.orm import relationship
@@ -16,6 +16,7 @@ from sqlalchemy.orm import relationship
 
 @login_manager.user_loader
 def load_user(id):
+    print(type(current_user))
     return User.query.get(int(id))
 
 
