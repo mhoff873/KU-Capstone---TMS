6e14efb67b42c467962566246e2039c69d3e8bc3
dkram389 <32393496+dkram389@users.noreply.github.com>
2018-03-09 00:11:43 -0500

    Test 4 cause I think i skipped to 5

--- views.py
+++ views.py
@@ -221,9 +221,9 @@ def user_assignment():
         users = UserMgmt.get_supervisor_users(current_user.email)
     else:
         users = User.query.all()
-    if validate_on_submit():
+    if form.add_task():
         tasks = Task.query.all()
-        return render_template("user_assignment.html", users=users, tasks=tasks, form=form)
+        #return render_template("user_assignment.html", users=users, tasks=tasks, form=form)
     else:
         tasks = None
     return render_template("user_assignment.html", users=users, tasks=tasks, form=form)
