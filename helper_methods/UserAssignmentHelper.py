9a28b3ca51ef485a94acc2d72461155c5155005e
dkram389 <32393496+dkram389@users.noreply.github.com>
2018-03-09 13:09:41 -0500

    stuff

--- helper_methods/UserAssignmentHelper.py
+++ helper_methods/UserAssignmentHelper.py
@@ -4,14 +4,24 @@ Author: Dylan Kramer
 Created 3/9/2018
 """
 
+from Forms.models import User, Supervisor, Request, Task
+from database import db
+
 def get_assignable_tasks(supervisorID=None):
     tasks = None
     if supervisorID is None:
         tasks = Task.query.filter_by(supervisorID=supervisorID).all()
     return tasks
 
+
 def get_tasks_assigned(userID=None):
     requests = None
     if userID is not None:
         requests = query.filter_by(userID)
     return requests
+
+
+def assign_task(user=None,task=None):
+    request = Request()
+
+    return
