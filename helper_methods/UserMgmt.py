684635d50c097d9ea176178741a822bfcf486932
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-22 12:39:37 -0400

    Testing.

--- helper_methods/UserMgmt.py
+++ helper_methods/UserMgmt.py
@@ -114,8 +114,10 @@ def get_supervisor_users(supervisor_email):
     :param supervisor_email: Email that is used to get the supervisor ID.
     :return: List of users that have the coordinating supervisor ID.
     """
+    print(supervisor_email)
     supervisorID = (Supervisor.query.filter_by(email=supervisor_email).first()).supervisorID
     users = User.query.filter_by(supervisorID=supervisorID).all()
+    print(users)
     return users
 
 
