878882952d3603de1709dc409f4312d7d44828cc
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-26 20:15:34 -0400

    Attempted rewrite of task assignment page.

--- views.py
+++ views.py
@@ -299,7 +299,7 @@ def user_assignment():
         users = User.query.all()
         print('Done querying ALL users')
     print([(user.userID, user.fname) for user in users])
-    form.assigned_users.process_data([(user.userID, user.fname) for user in users])
+    form.assigned_users.choices = [(user.userID, user.fname) for user in users]
     if form.assign_task_button.data:
         tasks = UserAssignmentHelper.get_assignable_tasks(current_user.supervisorID)
         return render_template("user_assignment.html", form=form, tasks=tasks)
