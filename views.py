3938e96d7a946a1e7f4b8f0b2033f2b06ed229cb
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-26 20:27:58 -0400

    All users added to dropdown.

--- views.py
+++ views.py
@@ -301,8 +301,10 @@ def user_assignment():
     # WARNING: If the user doesn't have a first name and a last name in the DB,
     # such as, say, the user was entered for testing purposes,
     # the concatenation of their first name and last name will crash the app.
-    print([(user.userID, user.fname + ' ' + user.lname) for user in users].append(('all_users', 'All Users')))
-    form.assigned_users.choices = [(user.userID, user.fname + ' ' + user.lname) for user in users].append(('all_users', 'All Users'))
+    print([(user.userID, user.fname + ' ' + user.lname) for user in users])
+    user_choices = [(user.userID, user.fname + ' ' + user.lname) for user in users]
+    user_choices.append(('all_users', 'All Users'))
+    form.assigned_users.choices = user_choices
     if form.assign_task_button.data:
         tasks = UserAssignmentHelper.get_assignable_tasks(current_user.supervisorID)
         return render_template("user_assignment.html", form=form, tasks=tasks)
