c82349247610deb8cd36af06744b2be535c9d347
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-29 19:54:03 -0400

    Removed all speech to text stuff. Python SpeechRecognition will not work server side and the HTML5 Web Speech API does not work on non-HTTPS websites.

--- views.py
+++ views.py
@@ -425,10 +425,10 @@ def create_task():
             main_step.detailed_steps.append_entry()
             return render_template('create_task.html', form=form)
         for j, detailed_step in enumerate(main_step.detailed_steps):
-            if detailed_step.voice_button_title.data:
-                detailed_step.title.process_data(TaskHelper.get_audio_transcript())
-            if detailed_step.voice_button_stepText.data:
-                detailed_step.stepText.process_data(TaskHelper.get_audio_transcript())
+            # if detailed_step.voice_button_title.data:
+            #     detailed_step.title.process_data(TaskHelper.get_audio_transcript())
+            # if detailed_step.voice_button_stepText.data:
+            #     detailed_step.stepText.process_data(TaskHelper.get_audio_transcript())
             if detailed_step.detailed_step_removal.data:
                 main_step.detailed_steps.entries.pop(j)
                 return render_template('create_task.html', form=form)
