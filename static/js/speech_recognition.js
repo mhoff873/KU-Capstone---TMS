5643b80d23ae96aa0481d8779e7058f8bbeee288
David Schaeffer <dscha959@live.kutztown.edu>
2018-03-30 09:28:19 -0400

    Removed check no longer needed.

--- static/js/speech_recognition.js
+++ static/js/speech_recognition.js
@@ -1,30 +1,25 @@
 function startDictation(element_id) {
     console.log('startDictation called.');
-    console.log(window.hasOwnProperty('webkitSpeechRecognition'));
-    if (window.hasOwnProperty('webkitSpeechRecognition')) {
-        console.log('Browser is HTML5 Speech Recognition capable');
-        var recognition = new new (window.SpeechRecognition ||
-            window.webkitSpeechRecognition ||
-            window.mozSpeechRecognition ||
-            window.msSpeechRecognition)();
-        console.log(recognition);
-        recognition.continuous = false;
-        recognition.interimResults = false;
+    var recognition = new new (window.SpeechRecognition ||
+        window.webkitSpeechRecognition ||
+        window.mozSpeechRecognition ||
+        window.msSpeechRecognition)();
+    console.log(recognition);
+    recognition.continuous = false;
+    recognition.interimResults = false;
 
-        recognition.lang = "en-US";
-        recognition.start();
-        console.log('Starting recording.');
-        recognition.onresult = function (e) {
-            console.log('Result: ' + e.results[0][0].transcript);
-            document.getElementById(element_id).value
-                = e.results[0][0].transcript;
-            recognition.stop();
-        };
+    recognition.lang = "en-US";
+    recognition.start();
+    console.log('Starting recording.');
+    recognition.onresult = function (e) {
+        console.log('Result: ' + e.results[0][0].transcript);
+        document.getElementById(element_id).value
+            = e.results[0][0].transcript;
+        recognition.stop();
+    };
 
-        recognition.onerror = function (e) {
-            console.log('Error occurred: ' + e);
-            recognition.stop();
-        }
-
+    recognition.onerror = function (e) {
+        console.log('Error occurred: ' + e);
+        recognition.stop();
     }
 }
