4a64e90adb4caaed14d800c89454fe21883edb15
David Schaeffer <dscha959@live.kutztown.edu>
2018-04-24 09:33:31 -0400

    Troubleshooting image handling logic.

--- views.py
+++ views.py
@@ -495,7 +495,18 @@ def edit_task(task_id=None):
                                    detailed_step_ids=detailed_step_ids)
     # Below code runs on POST requests.
     form = CreateTaskForm(request.form)
-
+    if task_id is not None:
+        print('TASK ID: ', task_id)
+        task_image = 'T={}'.format(task_id)
+        print('Task image: ', task_image)
+        main_step_ids = []  # Dict of main step IDs. keys are the main step number: 1, 2, 3, etc. NOT IDS
+        detailed_step_ids = []  # Dict of dict of detailed step IDs. {'main step number': {detailed step number: detailedStepID} }
+        for i, main_step in enumerate(TaskHelper.get_main_steps_for_task(task_id)):
+            main_step_ids[i+1] = 'M={}'.format(main_step.mainStepID)
+            print('Main Step image: ', main_step.mainStepID)
+            for j, detailed_step in enumerate(TaskHelper.get_detailed_steps_for_main_step(main_step.mainStepID)):
+                detailed_step_ids[i+1] = {j+1: 'D={}'.format(detailed_step.detailedStepID)}
+                print('Detailed Step image: ', detailed_step.detailedStepID)
     if form.save.data:
         """Save task as draft."""
         task = TaskHelper.create_task(form, request.files)
